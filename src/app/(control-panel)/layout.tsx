f3c34be6414c436437a615f7af19f9542fdc394e
zayn59970 <zayn59970@gmail.com>
2025-05-06 17:07:42 +0300

    Update layout.tsx

--- src/app/(control-panel)/layout.tsx
+++ src/app/(control-panel)/layout.tsx
@@ -1,7 +1,8 @@
 import MainLayout from 'src/components/MainLayout';
 import AuthGuardRedirect from '@auth/AuthGuardRedirect';
 
-function Layout({ children }) {
+function Layout({ children }) { 
+	
 	return (
 		<AuthGuardRedirect auth={['admin','user']}>
 			<MainLayout>{children}</MainLayout>
